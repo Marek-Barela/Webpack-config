96cde12531503d611efa48e7546c22019576ed28
Marek Bareła <marek.barela@wp.pl>
2019-09-24 00:51:51 +0200

    autoprefixer loader commited, file structure changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import Hello from "./files/hello";
 import "./styles/style.sass";
-
-const hello = new Hello("index");
-
-console.log(hello.sayHello());
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
       },
       {
         test: /\.(sass|scss)$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader","postcss-loader", "sass-loader"]
       }
     ]
   },
